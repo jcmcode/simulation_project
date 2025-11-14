b3e93390083f71e959d05595ce1f36d278dbd2bf
jcmcode <147959838+jcmcode@users.noreply.github.com>
2025-11-13 19:32:29 -0500

    update to world.py

--- core/world.py
+++ core/world.py
@@ -1,7 +1,19 @@
 import numpy  as np
 import pandas as pd
 
+
 class World:
+    def __init__(self):
+        pass
+    
+    
+    
+    
+class Agent:
+    def __init__(self):
+        pass
+    
+class Enviornment:
     def __init__(self):
         pass
     
